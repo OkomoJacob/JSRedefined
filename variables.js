4c363b9bff61dce2fa7772bac97be7f5dd0936f5
okomojacob <jaylabs2030@gmail.com>
2021-09-09 22:42:55 +0300

    :hammer:, code

---
+++ variables.js
@@ -0,0 +1,15 @@
+/* Variales */
+var mayName = "Okomo";
+console.log(mayName);
+
+let myFullName = "OkomoJacob";
+console.log(myFullName);
+
+const myAge = 23;
+console.log(myAge);
+
+// Declaring Vars
+const a;
+
+// Assigning Variables
+const a = 334;
